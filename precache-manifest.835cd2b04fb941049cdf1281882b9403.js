1b09674287631c3c530fab042a6855f519d0d6e3
Abhishek Singh <abhisheksingh@admins-MacBook-Pro.local>
2021-06-27 17:07:55 +0530

    Updates

---
+++ precache-manifest.835cd2b04fb941049cdf1281882b9403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3001e09ab8fadf01432b80a452576cd3",
+    "url": "/Pokedex-ReactJs/index.html"
+  },
+  {
+    "revision": "ba01ac0003c268652da2",
+    "url": "/Pokedex-ReactJs/static/css/main.ef12674b.chunk.css"
+  },
+  {
+    "revision": "88f34c558e51f890fb32",
+    "url": "/Pokedex-ReactJs/static/js/2.9be59bd9.chunk.js"
+  },
+  {
+    "revision": "ba01ac0003c268652da2",
+    "url": "/Pokedex-ReactJs/static/js/main.c6e82c6b.chunk.js"
+  },
+  {
+    "revision": "aee07cbc29612f191e2a",
+    "url": "/Pokedex-ReactJs/static/js/runtime~main.761340b3.js"
+  },
+  {
+    "revision": "7d7a6aa3ecd62e0897fc03ee76f202fa",
+    "url": "/Pokedex-ReactJs/static/media/poke.7d7a6aa3.csv"
+  }
+]);
