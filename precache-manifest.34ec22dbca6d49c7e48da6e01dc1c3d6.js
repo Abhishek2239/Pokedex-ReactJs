bd05d82d875cdd6b256fc8775abc414b1aa89db7
Abhishek Singh <abhisheksingh@admins-MacBook-Pro.local>
2021-06-27 16:08:52 +0530

    minot channge

---
+++ precache-manifest.34ec22dbca6d49c7e48da6e01dc1c3d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016759f24aa973b95e0baca7c679941d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1b10a2f32744062f02",
+    "url": "/static/css/main.ef12674b.chunk.css"
+  },
+  {
+    "revision": "88f34c558e51f890fb32",
+    "url": "/static/js/2.9be59bd9.chunk.js"
+  },
+  {
+    "revision": "6d1b10a2f32744062f02",
+    "url": "/static/js/main.0b83a240.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d7a6aa3ecd62e0897fc03ee76f202fa",
+    "url": "/static/media/poke.7d7a6aa3.csv"
+  }
+]);
